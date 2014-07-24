c2550737d385d0705e8623ef9ed22422fe0ef6f5
Chris Rohde <christopher.rohde@gmail.com>
2014-07-24 16:56:21 -0400

    Corrected BS4 Use
    A better understanding of beautifulsoup items and .find() function

--- scrapist.py
+++ scrapist.py
@@ -2,11 +2,13 @@ from bs4 import BeautifulSoup
 from datetime import datetime, date
 import requests
 
+# Sets date to current, formatted for USCCB site
 pull_date = datetime.strftime(datetime.today(), '%m%d%y')
-cath_response = requests.get('http://www.usccb.org/bible/readings/' + pull_date + '.cfm')
 
+cath_response = requests.get('http://www.usccb.org/bible/readings/' + pull_date + '.cfm')
 cath_soup = BeautifulSoup(cath_response.text)
 
-# print(cath_soup.prettify())
+# print cath_soup.prettify()
+
 for a in cath_soup.find_all("div", {"class": "bibleReadingsWrapper"}):
     b = BeautifulSoup(a.text)
